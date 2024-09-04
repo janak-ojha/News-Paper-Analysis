7b1da921ac1cee214e118b89a833582cbd8b7b3a
janak-ojha <janakrajojha66@gmail.com>
2024-09-04 19:18:23 +0530

    bug

--- preprocessing.py
+++ preprocessing.py
@@ -3,20 +3,13 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
 
-# Download necessary NLTK data files
-nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 def clean_article(article):
-    # Tokenize the article into words
     tokens = word_tokenize(article)
-    # Convert tokens to lowercase
     tokens = [token.lower() for token in tokens]
-    # Remove non-alphanumeric characters
     tokens = [re.sub(r'[^a-zA-Z0-9]', '', token) for token in tokens if token.isalnum()]
-    # Get the set of English stopwords
     stop_words = set(stopwords.words('english'))
-    # Remove stopwords from the tokens
     cleaned_tokens = [token for token in tokens if token not in stop_words]
-    # Return the cleaned article as a single string
     return ' '.join(cleaned_tokens)
